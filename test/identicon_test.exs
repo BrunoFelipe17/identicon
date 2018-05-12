db92d43c57f5f1648542e19812c114b7b0821b98
Bruno Felipe <bruno.felipe7170@gmail.com>
2018-05-12 19:29:38 -0300

    hash_input method created

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,7 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  test "Return a list with hashed string" do
+    assert Identicon.hash_input("test") == [9, 143, 107, 205, 70, 33, 211, 115, 202, 222, 78, 131, 38, 39, 180, 246]
   end
 end
